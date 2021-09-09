2e36eeb609b650a6c8602be1540da845ac598a50
PrabuElumalai <33721640+PrabuElumalai@users.noreply.github.com>
2021-09-09 03:28:16 -0400

    Fetched all the user full details in loop

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,31 @@ import UserList from "./UserList";
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
   const headers = { "headers": { "app-id": "6133558887895dca3fdc9a03" } };
   // retrieve cuser data from dummyapi
   const retriveUsers = async () => {
-    const response = await api.get("/data/v1/user?limit=50", headers);
-    //console.log(response.data.data);
+    const response = await api.get("/data/v1/user/?limit=5", headers);
+    console.log(response.data.data);
     return response.data.data;
+  };
+  const retriveUser = async (allUsers) => {
+    for (var i = 0; i < allUsers.length; i++) {
+      console.log(allUsers[i].id);
+      if (allUsers[i].id) {
+        const response = await api.get("/data/v1/user/" + allUsers[i].id, headers);
+        allUsers[i] = response.data;
+      }
+    }
+
+
+    return allUsers;
   };
   useEffect(() => {
     const getUsers = async () => {
       const allUsers = await retriveUsers();
       if (allUsers) setUsers(allUsers);
-      //console.log(allUsers);
+      retriveUser(allUsers);
     };
     getUsers();
 
@@ -34,12 +47,12 @@ function App() {
     <section>
       <div>
         {/* <Router> */}
-          <Header />
-          {/* <Switch> */}
-          {/* <Route path="/" exact component={UserList} />  */}
-          <UserList users={users}/>
-         {/* </Switch> */}
-          <Footer />
+        <Header />
+        {/* <Switch> */}
+        {/* <Route path="/" exact component={UserList} />  */}
+        <UserList users={users} />
+        {/* </Switch> */}
+        <Footer />
         {/* </Router> */}
       </div>
     </section>
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,20 +3,32 @@ import React from "react";
 const UserCard = (props) => {
     const { id, title, firstName, lastName, picture } = props.user;
     return (
-        <div className="col-sm-12 col-md-4 mb-4">
-            <div className="card testimonial-card" style={{ maxWidth: "22rem" }}>
-                <div className="card-up aqua-gradient"></div>
-                <div className="avatar mx-auto white">
+        <div className="col-sm-12 col-md-6 mb-4">
+            <div className="card testimonial-card">
+                <div className="row">
+                <div className="col-4 avatar ml-2 white">
                     <img src={picture} className="rounded-circle img-responsive mt-3" alt="woman avatar" style={{maxWidth:"75px"}}/>
+
                 </div>
-                <div className="card-body">
-                    <h4 className="card-title">{firstName} {lastName}</h4>
-                    <hr />
-                    <p><i className="fas fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos,
-                        adipisci</p>
+                <div className="col-6">
+                    <h6 className="mt-2 text-strong">{title} {firstName} {lastName}</h6>
+                </div>
+                </div>
+                <div className="card-body">                    
+                   <button>View Details</button>
                 </div>
             </div>
         </div>
+        // <div className="bg-white rounded shadow p-4 overflow-hidden flex md:flex-row flex-col">
+        //     <div className="flex-grow"><div className="truncate"><b>ID:</b> 60d0fe4f5311236168a109ca</div>
+        //     <img src="https://randomuser.me/api/portraits/women/58.jpg"/></div>
+        //     <div className="flex-grow"><div className="font-semibold text-lg truncate">ms. Sara Andersen</div>
+        //     <div className="truncate"><b>Gender:</b> female</div><div className="truncate"><b>Date of birth:</b> Apr 30 1996</div>
+        //     <div className="truncate"><b>Register date:</b> Jun 21 2021</div>
+        //     <br/><div className ="truncate"><b>Email: </b> sara.andersen @example.com</div>
+        //     <div className ="truncate"><b>Phone: </b> 92694011</div></div><div className="flex-grow"><b>Address</b><div className="truncate">
+        //         <b>State:</b> Nordjylland</div><div className="truncate"><b>Street:</b> 9614, SÃ¸ndermarksvej</div><div className="truncate">
+        //             <b>City:</b> Kongsvinger</div><div className="truncate"><b>Country:</b> Denmark</div><div className="truncate"><b>Timezone:</b> -9:00</div></div></div>
     );
 
 };
